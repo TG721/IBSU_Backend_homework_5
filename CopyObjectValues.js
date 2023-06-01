870229f9af0ff093c5c033fa40e1dbf841d1803d
Tengiz Gachechiladze <85778941+TG721@users.noreply.github.com>
2023-06-02 00:05:16 +0400

    Update CopyObjectValues.js
     now object is getting copied by values

--- CopyObjectValues.js
+++ CopyObjectValues.js
@@ -1,7 +1,5 @@
-export default function CopyObjectValues(source, target) {
-    for (let key in source) {
-      if (source.hasOwnProperty(key)) {
-        target[key] = source[key];
-      }
-    }
+import produce from 'immer';
+
+export default function copyObjectValues(source) {
+  return produce(source, (draft) => {});
 }
